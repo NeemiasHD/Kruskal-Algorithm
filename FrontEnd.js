965d8bf16c4695c02d77dea2f853a4125cde7888
Neemias Claudio <neemiasclaudio5@gmail.com>
2024-02-27 20:10:21 -0300

    concertando erros do front end

--- FrontEnd.js
+++ FrontEnd.js
@@ -2,7 +2,16 @@
 let g ;
 
 
+function voltarInicio(){
+    document.querySelector(".Container_Inputs").style.display = "flex"
+    document.querySelector(".AddAresta").style.display = "none"
+    document.querySelector(".VerticesDisponiveis").style.display ="none"; 
+    document.querySelector(".Vertices_Criados").style.display = "none"; 
+    document.querySelector(".Vertices_Criados").innerHTML = "<H1>VERTICES DISPONIVEIS</H1>"; 
+    document.querySelector(".VerticesDisponiveis").innerHTML ="<H1>VERTICES DISPONIVEIS</H1>"; 
+    document.querySelector(".Vertices_Criados.T2").style.display ="none"; 
 
+}
 function CriarAresta(){
     const Origem = document.querySelector(".InputOrigem").value;
     const Destino = document.querySelector(".InputDestino").value;
