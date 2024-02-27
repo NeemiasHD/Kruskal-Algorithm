ea3eeaadc3f662517f4559e05e8d732a93c97bc2
Neemias Claudio <neemiasclaudio5@gmail.com>
2024-02-27 19:54:16 -0300

    modulando um arquivo js para um proprio para o front end

---
+++ FrontEnd.js
@@ -0,0 +1,49 @@
+// Funções para o  Front End 
+let g ;
+
+
+
+function CriarAresta(){
+    const Origem = document.querySelector(".InputOrigem").value;
+    const Destino = document.querySelector(".InputDestino").value;
+    const Peso = document.querySelector(".InputPeso").value;
+    g.AdicionarAresta(Origem, Destino, Peso);
+    if(Origem > g.V-1|| Destino> g.V-1 || !Destino ||!Origem){
+        alert("VERTICE NÃO EXISTE");
+    }else{
+
+    
+    document.querySelector(".Vertices_Criados").innerHTML +=
+    `
+    <div class="VerticeCriadosContainer">
+    
+        <p class="OrigemContainer">Origem: <p class="Origem">${Origem}</p></p>
+        <p class="DestinoContainer">Destino: <p class="Destino">${Destino}</p></p>
+        <p class="PesoContainer">Peso: <p class="Peso">${Peso}</p></p>
+    </div>
+    `
+    document.querySelector(".Vertices_Criados").style.display = "flex"
+    }   
+
+}
+function CriarGrafo(){
+    const Vertices = parseInt(document.querySelector(".NdeVerticesInput").value)
+    document.querySelector(".Container_Inputs").style.display = "none"
+    document.querySelector(".AddAresta").style.display = "flex"
+    //Criando Grafo
+    g = new Grafo(Vertices);
+    document.querySelector(".VerticesDisponiveis").style.display = "flex"
+    for(let i = 0; i<Vertices;i++){
+        document.querySelector(".VerticesDisponiveis").innerHTML += `
+         <p>${i}</p>
+        `
+    }
+
+}
+function Calcular(){
+    
+    document.querySelector(".Vertices_Criados.T2").innerHTML = "";
+    //Chamando A função de Organizar Kruskal
+    g.kruskal();
+    document.querySelector(".Vertices_Criados.T2").style.display = "flex";
+}
--- Kruskal.js
+++ Kruskal.js
@@ -1,5 +1,3 @@
-let g ;
-
 
 class Aresta {
     constructor(origem, destino, peso) {
@@ -84,49 +82,3 @@ class Grafo {
 
 
 
-// Funções pro Front End 
-
-function CriarAresta(){
-    const Origem = document.querySelector(".InputOrigem").value;
-    const Destino = document.querySelector(".InputDestino").value;
-    const Peso = document.querySelector(".InputPeso").value;
-    g.AdicionarAresta(Origem, Destino, Peso);
-    if(Origem > g.V-1|| Destino> g.V-1 || !Destino ||!Origem){
-        alert("VERTICE NÃO EXISTE");
-    }else{
-
-    
-    document.querySelector(".Vertices_Criados").innerHTML +=
-    `
-    <div class="VerticeCriadosContainer">
-    
-        <p class="OrigemContainer">Origem: <p class="Origem">${Origem}</p></p>
-        <p class="DestinoContainer">Destino: <p class="Destino">${Destino}</p></p>
-        <p class="PesoContainer">Peso: <p class="Peso">${Peso}</p></p>
-    </div>
-    `
-    document.querySelector(".Vertices_Criados").style.display = "flex"
-    }   
-
-}
-function CriarGrafo(){
-    const Vertices = parseInt(document.querySelector(".NdeVerticesInput").value)
-    document.querySelector(".Container_Inputs").style.display = "none"
-    document.querySelector(".AddAresta").style.display = "flex"
-    //Criando Grafo
-    g = new Grafo(Vertices);
-    document.querySelector(".VerticesDisponiveis").style.display = "flex"
-    for(let i = 0; i<Vertices;i++){
-        document.querySelector(".VerticesDisponiveis").innerHTML += `
-         <p>${i}</p>
-        `
-    }
-
-}
-function Calcular(){
-    
-    document.querySelector(".Vertices_Criados.T2").innerHTML = "";
-    //Chamando A função de Organizar Kruskal
-    g.kruskal();
-    document.querySelector(".Vertices_Criados.T2").style.display = "flex";
-}
